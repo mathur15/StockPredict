cf3ffd3f6d0aa245206e6ad41401551d7413bd94
mathur15 <som.mathur1997@gmail.com>
2020-04-30 17:10:43 -0400

    Baseline model complete

---
+++ LastValue.py
@@ -0,0 +1,35 @@
+#predicting adj close price using LastValue Method
+    #-set current obs adj value to last observed value
+    #-Baseline model
+
+#import dataset
+
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+#import dataset 
+df = pd.read_csv('VTI.csv')
+df_adj_close = df['Adj Close']
+
+length = len(df_adj_close)
+
+#split up data to build model and validate
+split_point = length -60
+test,validate = df_adj_close[0:split_point],df_adj_close[split_point:]
+test.to_csv('test.csv',header=False)
+validate.to_csv('validate.csv',header=False)
+
+history = [x for x in test]
+predicted = list()
+for item in test:
+    y_hat = history[-1]
+    predicted.append(y_hat)
+    observed = item
+    history.append(observed)
+    print(">Predicted=%.3f   Expected=%.3f" %(y_hat,observed))
+    
+print("\n")
+print("Evaluating performance using mean squared error \n")
+
+print(mean_squared_error(test.values,predicted))
+
