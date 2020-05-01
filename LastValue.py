f9464bbba07874e501d2b5f1bc122f07cc357d5b
mathur15 <som.mathur1997@gmail.com>
2020-05-01 13:55:53 -0400

    Added code to plot and make comparison easier

--- LastValue.py
+++ LastValue.py
@@ -6,10 +6,13 @@
 
 import pandas as pd
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 #import dataset 
 df = pd.read_csv('VTI.csv')
 df_adj_close = df['Adj Close']
+
+df_adj_close_date = df[["Date",'Adj Close']]
 
 length = len(df_adj_close)
 
@@ -32,4 +35,10 @@ print("\n")
 print("Evaluating performance using mean squared error \n")
 
 print(mean_squared_error(test.values,predicted))
+
+ax = df_adj_close_date.plot(x='Date',grid=True,style='b-')
+plt.show()
+df_predicted = pd.DataFrame(predicted)
+ax1 = df_predicted.plot(grid=True,style='r-')
+plt.show()
 
